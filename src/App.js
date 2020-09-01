55b0ff8715bf4b455530414409c3df85191a365e
Murad Cholukov <muradusa@gmail.com>
2020-09-01 16:06:58 -0700

    setting up onChange listeters for form inputs

--- src/App.js
+++ src/App.js
@@ -1,11 +1,48 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
 import './App.css';
 
 function App() {
+
+  const [input, setInput] = useState("");
+  const [file, setFile] = useState("");
+
+
+  const handleInputChange = (e) => {
+    e.preventDefault()
+    setInput(e.target.value)
+  }
+
+  const handleFileChange = e => {
+    e.preventDefault()
+    setFile(e.target.file)
+  }
+
+  const handleUpload = (e) => {
+
+  }
+
+ 
+
   return (
     <div className="App">
-      <h1>Test Page</h1>
+      <form className="app__form" action="submit">
+        <input 
+        type="text"
+        value={input}
+        onChange={handleInputChange}
+        />
+        <input 
+        type="file"
+        value={file}
+        onChange={handleFileChange}
+        />
+        <button
+        onClick={handleUpload}
+        >
+          Upload
+        </button>
+      </form>
     </div>
   );
 }
